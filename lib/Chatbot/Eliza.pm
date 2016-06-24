8aac13ec9b56b05a806f16d016dec588d693fc1c
Lnation <thisusedtobeanemail@gmail.com>
2016-06-24 21:25:03 +0100

    sane way of initiating eliza

--- lib/Chatbot/Eliza.pm
+++ lib/Chatbot/Eliza.pm
@@ -11,21 +11,42 @@ use experimental qw[
 	lexical_subs
 ];
 
+my @user_options = qw(name scriptfile debug prompts_on memory_on myrand);
+foreach my $field (@user_options) {
+	has $field => (
+		is => 'rw',
+		default => undef,
+	);
+}
+
 has 'options' => (
 	is => 'rw',
 	lazy => 1,
+	builder => '_build_options',
 	default => sub {
 		return Chatbot::Eliza::Option->new();
 	}
 );
 
+sub _build_options ($self) {
+	my $options = Chatbot::Eliza::Option->new();
+	foreach my $field (@user_options) {
+		if (my $val = $self->$field) {
+			$options->$field($val);
+		}
+	}
+	return $options;
+}
+
 has 'eliza' => (
 	is 	=> 'rw',
 	lazy => 1,
-	default => sub { 
-		Chatbot::Eliza::Brain->new(options => $self->options);	
-	}
+	builder => '_build_eliza'
 );
+
+sub _build_eliza ($self) {
+	return Chatbot::Eliza::Brain->new(options => $self->options);
+}
 
 sub command_interface {
 
